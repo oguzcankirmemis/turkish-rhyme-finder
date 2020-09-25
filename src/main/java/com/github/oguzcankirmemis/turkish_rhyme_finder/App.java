6c3aeb0157d766becb0ed8011c2decd2c31df75c
Oguzcan Kirmemis <oguzcan.kirmemis@gmail.com>
2020-09-26 01:50:47 +0300

    Added initial project files

---
+++ src/main/java/com/github/oguzcankirmemis/turkish_rhyme_finder/App.java
@@ -0,0 +1,13 @@
+package com.github.oguzcankirmemis.turkish_rhyme_finder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
